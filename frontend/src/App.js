1fcac3731ac14096f46f5ef3f96aafc54d3c44b5
Shibajyoti Maity <shibajyoti.maity06@gmail.com>
2025-10-18 01:39:40 +0530

    yedo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          learn me re babababababba
+          aidefnkedn
         </a>
       </header>
     </div>
